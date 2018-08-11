849100371acf2f099d8b53a32d0aec5cb33ec37f
Johan-1 <nilssonjohan10@gmail.com>
2018-08-11 13:18:40 +0200

    add build script so lavels dont have to be manually copied to the folder with the .exe

---
+++ Assets/Editor/PostBuild.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class PostBuild 
+{
+	[PostProcessBuild(1)]
+	public static void CopyLevelsToBuild(BuildTarget target, string buildPath)
+	{
+		FileUtil.ReplaceDirectory(Constants.TILEMAP_SAVE_FOLDER, System.IO.Path.GetDirectoryName(buildPath) + "/Maps");
+	}
+	
+}
